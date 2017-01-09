b649e26a9ed069d6af78559228d0e8c7492cf0ba
Julien Chanseaume <julien.chanseaume@gmail.com>
2017-01-09 16:43:37 +0100

    Refactored ColissimoConfigValue model

--- local/modules/Colissimo/Model/Config/ColissimoConfigValue.php
+++ local/modules/Colissimo/Model/Config/ColissimoConfigValue.php
@@ -12,13 +12,40 @@
 
 namespace Colissimo\Model\Config;
 
-use Colissimo\Model\Config\Base\ColissimoConfigValue as BaseColissimoConfigValue;
+use Colissimo\Colissimo;
+use Thelia\Core\Translation\Translator;
 
 /**
  * Class Colissimo
  * @package Colissimo\Model\Config
  * @author Thomas Arnaud <tarnaud@openstudio.fr>
  */
-class ColissimoConfigValue extends BaseColissimoConfigValue
+class ColissimoConfigValue
 {
+    const FREE_SHIPPING = 'free_shipping';
+    const PRICES = 'prices';
+    const ENABLED = 'enabled';
+    const EXPORT_TYPE = 'export_type';
+    const ACCOUNT_NUMBER = 'account_number';
+    const SENDER_CODE = 'sender_code';
+    const DEFAULT_PRODUCT = 'default_product';
+
+    const EXPORT_COLISHIP = 'coliship';
+    const EXPORT_EXPEDITOR = 'expeditor';
+
+    public static function getProducts()
+    {
+        $t = Translator::getInstance();
+
+        $products = [
+            'DOM' => $t->trans('Colissimo Domicile - sans signature', [], Colissimo::DOMAIN_NAME),
+            'DOS' => $t->trans('Colissimo Domicile - avec signature', [], Colissimo::DOMAIN_NAME),
+            'COM' => $t->trans('Colissimo Domicile Outre-Mer - sans signature', [], Colissimo::DOMAIN_NAME),
+            'CDS' => $t->trans('Colissimo Domicile Outre-Mer - avec signature', [], Colissimo::DOMAIN_NAME),
+            'ECO' => $t->trans('Colissimo Eco Outre-Mer', [], Colissimo::DOMAIN_NAME),
+            'COLI' => $t->trans('International Colissimo Expert (outside Europe)', [], Colissimo::DOMAIN_NAME),
+        ];
+
+        return $products;
+    }
 }
